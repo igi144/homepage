859024377f53b3fb0f54fd543e3cace8e439b3e5
Igor Pelczarski <igorpe144@gmail.com>
2022-12-13 20:10:09 +0100

    js changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 {
-    const button__logo = document.querySelector(".button__logo");
-    const header = document.querySelector(".header")
-    const button = document.querySelector(".button__background");
-    const body = document.querySelector(".body");
-    const themeName = document.querySelector(".themeName")
-
-    button__logo.addEventListener("click", () => {
-        header.remove();
-    });
-
     const toggleBackgroundColor = () => {
+
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".themeName");
 
         body.classList.toggle("dark");
 
         themeName.innerText = body.classList.contains("dark") ? "jasny" : "kolorowy";
     }
 
-    button.addEventListener("click", toggleBackgroundColor);
+    const init = () => {
+        const buttonLogo = document.querySelector(".button__logo");
+        const header = document.querySelector(".header")
+        const button = document.querySelector(".button__background");
+
+        button.addEventListener("click", toggleBackgroundColor);
+
+        buttonLogo.addEventListener("click", () => {
+            header.remove();
+        })
+    };
+    init();
 }
 
